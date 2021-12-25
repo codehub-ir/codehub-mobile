0eb599dd098c9a8731f45c8867fabee222269405
mahmoud <eslamimahmoud98@gmail.com>
2021-12-25 14:59:28 +0330

    vazir font added

--- lib/constant/app_theme/app_theme.dart
+++ lib/constant/app_theme/app_theme.dart
@@ -2,5 +2,8 @@ import 'package:codehub/constant/app_colors/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final theme = ThemeData(primaryColor: primaryColor);
+  static final theme = ThemeData(
+    primaryColor: primaryColor,
+    fontFamily: "vazir",
+  );
 }
