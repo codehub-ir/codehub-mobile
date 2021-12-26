7d1c1c84ba3c1cd579abc679a040184041ce2696
mahmoud <eslamimahmoud98@gmail.com>
2021-12-26 21:24:55 +0330

    size config files added and initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:codehub/constant/app_strings/app_strings.dart';
 import 'package:codehub/constant/app_theme/app_theme.dart';
 import 'package:codehub/screens/splash/ui/splash_screen.dart';
+import 'package:codehub/utils/size_config/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -13,20 +14,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: appTitle,
-      theme: AppTheme.theme,
-      localizationsDelegates: const [
-        GlobalCupertinoLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale("fa", "IR"), // OR Locale('ar', 'AE') OR Other RTL locales
-      ],
-      locale: const Locale("fa", "IR"),
-      home: const SplashScreen(),
+    return LayoutBuilder(
+      builder: (context, constraints) => OrientationBuilder(
+        builder: (context, orientation) {
+          SizeConfig().init(constraints, orientation);
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: appTitle,
+            theme: AppTheme.theme,
+            localizationsDelegates: const [
+              GlobalCupertinoLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale("fa", "IR"),
+              // OR Locale('ar', 'AE') OR Other RTL locales
+            ],
+            locale: const Locale("fa", "IR"),
+            home: const SplashScreen(),
+          );
+        },
+      ),
     );
   }
 }
