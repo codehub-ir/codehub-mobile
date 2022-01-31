ce44cd97e73f3acdb5739e8ceaefad48c2f4a81c
mahmoud <eslamimahmoud98@gmail.com>
2022-01-31 22:46:52 +0330

    dependencies injected

--- lib/src/injector.dart
+++ lib/src/injector.dart
@@ -1,6 +1,11 @@
 import 'package:codehub/services/storage/storage.dart';
+import 'package:codehub/src/data/datasources/remote/api_service.dart';
+import 'package:codehub/src/data/repositories/event_repository_impl.dart';
+import 'package:codehub/src/domain/usecases/get_events_usecase.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:http/http.dart';
 
 final getIt = GetIt.instance;
 
@@ -12,4 +17,16 @@ void serviceLocator() async {
   // storage
   getIt.registerSingleton<GetStorage>(storage);
   getIt.registerSingleton<Storage>(storageClass);
+
+  // inject dio instance
+  getIt.registerSingleton<Dio>(Dio());
+
+  // inject api service
+  getIt.registerSingleton<ApiService>(ApiService(getIt()));
+
+  // inject repositories
+  getIt.registerSingleton<EventRepositoryImpl>(EventRepositoryImpl(getIt()));
+
+  // inject usecases
+  getIt.registerSingleton<GetEventsUseCase>(GetEventsUseCase(getIt()));
 }
